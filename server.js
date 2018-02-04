59b3ab9ff7ea136d350a8f82e388c057194bdb4a
Sayan Mukherjee <sayan.mukherjee108@gmail.com>
2018-02-03 21:56:31 -0500

    setup express server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.use('/public', express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/app/src/templates/portfolio.html');
+});
+
+app.get('*', (req, res) => {
+    res.status(404).json({error: "404 Page Not Found"});
+});
+
+app.listen(5000, () => {
+    console.log('Dev app listening on port 5000!');
+});
